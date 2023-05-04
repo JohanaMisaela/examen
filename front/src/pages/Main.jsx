7d7206db526388fe2472d2faf142eabff4adbf49
Johanna Misaela <116675181+JohanaMisaela@users.noreply.github.com>
2023-05-05 00:25:44 +0300

    card'

--- front/src/pages/Main.jsx
+++ front/src/pages/Main.jsx
@@ -2,13 +2,24 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
 import Footer from '../components/Footer'
+import Cardlist from '../components/Cardlist'
 
 function Main() {
   return (
     <div>
         <Navbar />
-        <Sidebar/>
+        <div className='col'>
+          <div className="row1">
+          <Sidebar/>
+          </div>
+          <div className="row2">
+        <Cardlist/>
+          </div>
+        </div>
+        <div>
         <Footer />
+
+        </div>
     </div>
   )
 }
